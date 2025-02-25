5b5f465c33b2586e536459965f6b45e7e9d4f5ea
EmanoelCariolando <emanoelcariolando@gmail.com>
2025-02-24 21:29:02 -0300

    New

--- src/new.ts
+++ src/new.ts
@@ -1,20 +1,43 @@
-let numberOne = document.getElementById('one') as HTMLInputElement | null;
-let numberTwo = document.getElementById('two') as HTMLInputElement | null;
-let button = document.getElementById('add');
-let result = document.getElementById('result');
+   //-----------------------------CLASS NOTES-----------------------------\\
 
-function calc(n1: number, n2: number) {
-  return n1 + n2;
-}
+         //------------------- declare variables -------------------\\
 
-// Verifica se todos os elementos existem antes de adicionar o evento
-if (button && numberOne && numberTwo && result) {
-  button.addEventListener('click', () => {
-    let n1 = Number(numberOne!.value); // "!" garante que não é null
-    let n2 = Number(numberTwo!.value);
+  let nick: string = 'manel'  //declare a variable type string
+  let number: number = 10     //declare a variable type number
+  let boolean: boolean = true //declare a variable type boolean
 
-    result!.innerHTML = calc(n1, n2).toString();
+          //------------------- declare arrays -------------------\\
+
+  let array: number[] = [1, 2, 3, 4, 5]            //declare a variable type array of numbers
+  let names: string[] = ['manel', 'jose', 'pedro'] //declare a variable type array of strings
+  let values: any = ['manel', 'jose', 'pedro']     //declare a variable type array of strings or arrays type numbers
+
+          //------------------- function example -------------------\\
+  function firstUp(neime: string): string {  // < string of return 
+    let letter = neime.charAt(0).toUpperCase();
+    return letter + neime.substring(1);
+  }
+
+  let Newnames = ['manel', 'jose', 'pedro', 'jesus', 90] // Even if there is a number in the array, it will work because of if
+
+  names.forEach((name) => {
+        if(typeof name  === 'string'){
+        console.log(name.toUpperCase());
+   }
   });
-} else {
-  console.error("Erro: Um dos elementos não foi encontrado no DOM.");
-}
+
+
+  function takeInfo(user: {name: string, age?: number}){
+
+        if(user.age !== undefined){
+        return `seu nome é ${user.name} e você tem ${user.age} anos`
+        }else{
+        return `seu nome é ${user.name}`
+     }};
+     
+
+  let persona = {
+        name: 'manel',
+        age: 25
+  }
+  console.log(takeInfo(persona))
